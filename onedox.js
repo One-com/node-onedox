22eee85eeaac31eab2a259026112a989a562f90a
Morten Siebuhr <msi@one.com>
2012-08-16 11:51:04 +0200

    Fix mkdirp is async.

--- onedox.js
+++ onedox.js
@@ -29,55 +29,52 @@ var argv = optimist
 /*
  * Create output directory
  */
-mkdirp(argv.out);
+var staticDir = path.join(argv.out, 'static');
+mkdirp(staticDir, function () {
+    /*
+     * Run each file through DOX and figure out paths.
+     */
+    var files = [],
+        linkList = {};
+    argv._.forEach(function (file) {
+        try {
+            var docs = dox.parseComments(fs.readFileSync(file, 'utf-8'));
+        } catch (e) {
+            console.error("✗ Couldn't parse", file);
+            return;
+        }
 
-/*
- * Read files and DOX them.
- */
-var files = [],
-    linkList = {};
-
-/*
- * Run each file through DOX and figure out paths.
- */
-argv._.forEach(function (file) {
-    try {
-        var docs = dox.parseComments(fs.readFileSync(file, 'utf-8'));
-    } catch (e) {
-        console.error("✗ Couldn't parse", file);
-        return;
-    }
-
-    var outFile = file.replace(/\.js$/, '').replace(/\//g, '_') + '.html';
-    linkList[file] = outFile;
-    files.push({
-        source: file,
-        dox: docs,
-        outFile: path.join(argv.out, outFile)
+        var outFile = file.replace(/\.js$/, '').replace(/\//g, '_') + '.html';
+        linkList[file] = outFile;
+        files.push({
+            source: file,
+            dox: docs,
+            outFile: path.join(argv.out, outFile)
+        });
     });
-});
 
-/*
- * Run each file through the template and write it out.
- */
-files.forEach(function (file) {
-    fs.writeFileSync(file.outFile, template.render({
-        docs: file.dox,
-        menu: linkList,
-        title: file.source
-    }));
-    console.log("✓ Wrote", file.outFile);
-});
+    /*
+     * Run each file through the template and write it out.
+     */
+    files.forEach(function (file) {
+        fs.writeFileSync(file.outFile, template.render({
+            docs: file.dox,
+            menu: linkList,
+            title: file.source
+        }));
+        console.log("✓ Wrote", file.outFile);
+    });
 
-/*
- * Create an index.html, if there's no index.js.
- */
-if (!('index.js' in linkList)) {
-    var index = path.join(argv.out, "index.html");
-    fs.writeFileSync(index, template.render({
-        docs: [],
-        menu: linkList,
-        title: "Documentation index"
-    }));
-    console.log("✓ Wrote", index, "(no index.js to use as entry point)");
-}
+    /*
+     * Create an index.html, if there's no index.js.
+     */
+    if (!('index.js' in linkList)) {
+        var index = path.join(argv.out, "index.html");
+        fs.writeFileSync(index, template.render({
+            docs: [],
+            menu: linkList,
+            title: "Documentation index"
+        }));
+        console.log("✓ Wrote", index, "(no index.js to use as entry point)");
+    }
+});
